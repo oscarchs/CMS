33694182a234f5ae38112dcc6bac3c9b0bf32482
oscarchs <oscarchs.94@gmail.com>
2015-10-17 22:47:35 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,69 @@
+#!flask/bin/python
+import os
+import unittest
+from flask import (
+    Blueprint,
+    request,
+    render_template,
+    flash,
+    g,
+    session,
+    redirect,
+    url_for
+)
+from app import app, db
+from app.article.models import Article
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+        ctx = app.app_context()
+        ctx.push()
+        return app
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def testarticle(self):
+
+        test = app.test_client(self)
+        response = test.get('/art/delete/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        assert 'token' in session
+        self.assertTrue(User.verify_token(session['token'])!=None)
+    def testarticledeletepost(self):
+        user = User(username='testuser',
+        email='test@example.com',
+        password='test',
+        role=1,
+        status=1
+        )   
+        db.session.add(user)
+        db.session.commit()
+        usertest = User.query.filter(User.username == 'Testuser').first()
+        testarticle = Article(title='testtitle',
+                        	body='testbody',
+                        	created='01-01-2015',
+                        	section_name='testname',
+                        	section='testsection',
+                        	user_name=usertest.username,
+                        	user=usertest,
+                        	)
+        db.session.add(section_todelete)
+        db.session.commit()
+        self.assertTrue(testarticle.all()!= None)
+        self.assertTrue(testarticle.find_by_id()!= None)
+        self.assertTrue(testarticle.find_by_author(usertest.username)!= None)
+        self.assertTrue(testarticle.find_by_section('testsection')!= None)
+        self.assertTrue(testarticle.slug()!= None)
+        self.assertTrue(testarticle.create_in_words()!= None)
+
+if __name__ == '__main__':
+    unittest.main()
